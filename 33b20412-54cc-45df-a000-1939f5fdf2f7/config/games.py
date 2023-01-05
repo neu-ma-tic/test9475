8b3db59d7ffb6ac5c7c0bef7cc7f81dfc317fa68
neumatic <betxneu@gmail.com>
2023-01-05 02:17:58 +0000

    auto [Thu 05 Jan 2023 02:17:58 AM UTC]

---
+++ 33b20412-54cc-45df-a000-1939f5fdf2f7/config/games.py
@@ -0,0 +1,21 @@
+import discord
+
+__games__ = [
+    (discord.ActivityType.playing, 'with Eddy-Senpai'),
+    (discord.ActivityType.playing, 'with Cats'),
+    (discord.ActivityType.playing, 'try :help'),
+    (discord.ActivityType.playing, 'try :about'),
+    (discord.ActivityType.playing, 'with VS Code'),
+    (discord.ActivityType.playing, 'with Python'),
+    (discord.ActivityType.playing, 'with async'),
+    (discord.ActivityType.playing, 'with Karen-chan'),
+    (discord.ActivityType.playing, 'with Hinata-chan'),
+    (discord.ActivityType.playing, 'with Eduard Laser'),
+    (discord.ActivityType.watching, 'over {guilds} Server'),
+    (discord.ActivityType.watching, 'over {members} Members'),
+    (discord.ActivityType.watching, 'Trash Animes'),
+    (discord.ActivityType.watching, 'you right now'),
+    (discord.ActivityType.watching, 'Hentai'),
+    (discord.ActivityType.listening, 'Podcasts')
+]
+__gamesTimer__ = 2 * 60 #2 minutes
---
+++ 33b20412-54cc-45df-a000-1939f5fdf2f7/loadconfig.py
@@ -0,0 +1,42 @@
+import os
+
+configFile = os.path.join('.', 'config', 'config.py')
+
+if os.path.isfile(configFile):
+    try:
+        from config.config import __token__
+    except ImportError:
+        raise Exception('__token__ variable MUST be set ')
+    try:
+        from config.config import __prefix__
+    except ImportError:
+        __prefix__ = ':'
+    try:
+        from config.config import __botserverid__
+    except ImportError:
+        __botserverid__ = 0
+    try:
+        from config.config import __greetmsg__
+    except ImportError:
+        __greetmsg__ = ''
+    try:
+        from config.config import __leavemsg__
+    except ImportError:
+        __leavemsg__ = ''
+    try:
+        from config.config import __kawaiichannel__
+    except ImportError:
+        __kawaiichannel__ = 0
+else:
+    #Fallback for Heroku or Docker environments
+    __token__ = os.environ.get('DISCORD_TOKEN')
+    __prefix__ = os.environ.get('DISCORD_PREFIX')
+    __botserverid__ = int(os.environ.get('DISCORD_BOTSERVERID'))
+    __kawaiichannel__ = int(os.environ.get('DISCORD_KAWAIICHANNEL'))
+    __greetmsg__ = os.environ.get('DISCORD_GREETMSG')
+    __leavemsg__ = os.environ.get('DISCORD_LEAVEMSG')
+    
+
+from config.games import __games__, __gamesTimer__
+from config.cogs import __cogs__
+from config.blacklist import __blacklist__
