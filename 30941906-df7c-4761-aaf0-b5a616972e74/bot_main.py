4f056e3abfb84d04bc1df1f82a87b4f088612bb7
neumatic <betxneu@gmail.com>
2023-01-05 01:50:06 +0000

    auto [Thu 05 Jan 2023 01:50:06 AM UTC]

---
+++ 30941906-df7c-4761-aaf0-b5a616972e74/bot_main.py
@@ -0,0 +1,57 @@
+import asyncio
+import os
+
+import discord
+from keep_alive import keep_alive
+from dotenv import load_dotenv
+from discord.ext import commands
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+GUILD = os.getenv("GUILD")
+ID_MARIUSZA = os.getenv("ID_MARIUSZA")
+
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
+channel_leszke = discord.utils.get(client.get_all_channels(), name="Leszke")
+
+
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.invisible, activity=None)
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(f'{client.user} is connected to guild {guild.name}, {guild.id}')
+
+    members = '\n - '.join([str(member.id) for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("!members"):
+        for guild in client.guilds:
+            if guild.name == GUILD:
+                for member in guild.members:
+                    print(member.name, member.id)
+
+
+@client.event
+async def on_voice_state_update(member, before, after):
+    if member.id == id_mariusza:
+        if after.channel:
+            await member.move_to(channel_leszke)
+
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Siema {member.name}, witaj na Analnym Kanale, gdzie czeka na Ciebie mnóstwo spierdolenia i analnej rozkoszy.'
+            )
+
+keep_alive()
+client.run(TOKEN)
---
+++ 30941906-df7c-4761-aaf0-b5a616972e74/main.py
@@ -0,0 +1,69 @@
+import os
+
+import discord
+from discord.ext import commands
+from keep_alive import keep_alive
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+GUILD = os.getenv("GUILD")
+ID_MARIUSZA = int(os.getenv("ID_MARIUSZA"))
+
+print(ID_MARIUSZA)
+
+intents = discord.Intents.default()
+intents.members = True
+#client = discord.Client(intents=intents)
+client = commands.Bot(command_prefix='!', intents=intents)
+channel_leszke = discord.utils.get(client.get_all_channels(), name="Leszke")
+
+
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.invisible, activity=None)
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(f'{client.user} is connected to guild {guild.name}, {guild.id}')
+
+    members = '\n - '.join([str(member.id) for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+
+# @client.event
+# async def on_message(message):
+#     if message.content.startswith("!members"):
+#         for guild in client.guilds:
+#             if guild.name == GUILD:
+#                 for member in guild.members:
+#                     print(member.name, member.id)
+
+    ## elif message.content.startswith("!alive"):
+    #   await message.channel.send("I am alive... but at what cost? <:FeelsBadMan:777141716826062879>")
+
+
+@client.command()
+async def alive(ctx):
+  await ctx.send("I am alive... but at what cost? <:FeelsBadMan:777141716826062879>")
+
+
+@client.event
+async def on_voice_state_update(member, before, after):
+  if member.id == ID_MARIUSZA:
+    if after.channel:
+        await member.move_to(channel_leszke)
+
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Siema {member.name}, witaj na Analnym Kanale, gdzie czeka na Ciebie mnóstwo spierdolenia i analnej rozkoszy.'
+            )
+
+
+keep_alive()
+client.run(TOKEN)
