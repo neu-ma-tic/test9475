3ab457b9577023395520058826aee1aebc0a1e26
neumatic <betxneu@gmail.com>
2023-01-05 01:59:03 +0000

    auto [Thu 05 Jan 2023 01:59:03 AM UTC]

---
+++ 875d794b-5906-482a-9f6c-cbb65645eac2/index.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const mySecret = process.env['TOKEN']
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+})
+
+client.on("message", msg => {
+  if(msg.content === 'ping') {
+    msg.reply('pong');
+  }
+})
+
+client.login(mySecret);
+
