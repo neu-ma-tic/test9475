c42f91b45f9deec41d087a06119c27cc329314b5
neumatic <betxneu@gmail.com>
2023-01-05 01:50:59 +0000

    auto [Thu 05 Jan 2023 01:50:59 AM UTC]

---
+++ 630b31d7-7e0b-4fb8-b64f-9d589735d3ed/config.go
@@ -0,0 +1,11 @@
+package main
+
+/*
+I needed to do this because when I was building the go binary it wasn't able to read the config.json from the service file in the way I was running it.
+*/
+
+// Token - Discord bot token, change from here
+var Token = "NjM3NzA2NDc0MDcxNjU0NDMz.XbSEbw.BUDL-_JluIArBY1KGA01omtycAc"
+
+// Prefix - The prefix the discord bot will respond to.
+var Prefix = "."
