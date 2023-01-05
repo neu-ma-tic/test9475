f525a68e720431b28417aa884b15d79c2c66c004
neumatic <betxneu@gmail.com>
2023-01-05 01:56:38 +0000

    auto [Thu 05 Jan 2023 01:56:38 AM UTC]

---
+++ 288cab96-62ea-44d9-aeef-bb76023fd1f9/main.py
@@ -0,0 +1,18 @@
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(os.getenv '5a914d3a9597bde30adb1e48f4cb861c2c8369f9323b1916a3b1ea7d192f417a'))
