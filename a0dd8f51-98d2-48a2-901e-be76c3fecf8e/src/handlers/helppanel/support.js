b7aa0e4a1e7c9c0f42a6e43b36905c779b479251
neumatic <betxneu@gmail.com>
2023-01-05 02:09:56 +0000

    auto [Thu 05 Jan 2023 02:09:56 AM UTC]

---
+++ a0dd8f51-98d2-48a2-901e-be76c3fecf8e/src/handlers/helppanel/support.js
@@ -0,0 +1,64 @@
+const Discord = require('discord.js');
+
+module.exports = async (client) => {
+    client.on('interactionCreate', async (interaction) => {
+        if (!interaction.isSelectMenu()) return;
+
+        if (interaction.customId == "Bot-helppanel") {
+            if (interaction.values == "support-Bothelp") {
+                interaction.deferUpdate();
+
+                const row2 = new Discord.MessageActionRow()
+                    .addComponents(
+                        new Discord.MessageSelectMenu()
+                            .setCustomId('Bot-helppanel')
+                            .setPlaceholder('❌┆Nothing selected')
+                            .addOptions([
+                                {
+                                    label: `Commands`,
+                                    description: `Show the commands of Bot!`,
+                                    emoji: "💻",
+                                    value: "commands-Bothelp",
+                                },
+                                {
+                                    label: `Invite`,
+                                    description: `Invite Bot to your server`,
+                                    emoji: "📨",
+                                    value: "invite-Bothelp",
+                                },
+                                {
+                                    label: `Support server`,
+                                    description: `Join the suppport server`,
+                                    emoji: "❓",
+                                    value: "support-Bothelp",
+                                },
+                                {
+                                    label: `Changelogs`,
+                                    description: `Show the bot changelogs`,
+                                    emoji: "📃",
+                                    value: "changelogs-Bothelp",
+                                },
+                            ]),
+                    );
+
+                let row = new Discord.MessageActionRow()
+                    .addComponents(
+                        new Discord.MessageButton()
+                            .setLabel("Support server")
+                            .setURL("https://discord.gg/Ds5CSrGfSW")
+                            .setStyle("LINK"),
+                    );
+
+                client.embed({
+                    title: `❓・Support`,
+                    desc: `Make your server even better with Bot!`,
+                    url: "https://discord.gg/Ds5CSrGfSW",
+                    components: [row2, row],
+                    type: 'edit'
+                }, interaction.message)
+            }
+        }
+    }).setMaxListeners(0);
+}
+
+ 
