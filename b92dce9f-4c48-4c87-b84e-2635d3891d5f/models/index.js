dd1571dd4c3065dbce99f55153e2012235fa8688
neumatic <betxneu@gmail.com>
2023-01-05 01:38:51 +0000

    auto [Thu 05 Jan 2023 01:38:51 AM UTC]

---
+++ b92dce9f-4c48-4c87-b84e-2635d3891d5f/models/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+    Guild: require("./Guild"),
+    User: require("./User"),
+    Member: require("./Member"),
+    Channel: require("./Channel"),
+    Message: require("./Message"),
+    Event: require("./Event"),
+    Poll: require("./Poll"),
+    RoleMenu: require("./RoleMenu"),
+    SoundEffect: require("./SoundEffect"),
+    Reminder: require("./Reminder")
+};
