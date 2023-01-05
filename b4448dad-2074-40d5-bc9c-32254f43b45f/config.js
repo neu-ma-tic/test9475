9abb0ddf3948974e6451879caee62da0e4f67013
neumatic <betxneu@gmail.com>
2023-01-05 02:17:10 +0000

    auto [Thu 05 Jan 2023 02:17:10 AM UTC]

---
+++ b4448dad-2074-40d5-bc9c-32254f43b45f/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    owner: '929933466819252284',
+    prefix: 'm-',
+    defaultGuildSettings: {
+        prefix: 'm-',
+        welcomeMessage: {
+            enabled: true,
+            welcomeMessage: "Welcome **{{member}}** to **{{server}}**!",
+            channelID: '1019094385486471188'
+        },
+        soundboardRole: "Soundboard DJ",
+        modRole: "Moderator",
+        adminRole: "Co-owner",
+        logChannel: "⌠😂⌡logs"
+    }
+}
